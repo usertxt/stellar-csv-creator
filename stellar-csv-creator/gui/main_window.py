c5a36843f400278c9cfd2872c5a104cb95436135
usertxt <digitaluser.txt@gmail.com>
2019-08-26 17:04:11 -0500

    Increase width of main window

--- stellar-csv-creator/gui/main_window.py
+++ stellar-csv-creator/gui/main_window.py
@@ -4,7 +4,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(585, 375)
+        MainWindow.resize(600, 375)
         MainWindow.setMinimumSize(QtCore.QSize(0, 0))
         MainWindow.setWindowTitle("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -224,7 +224,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_5.addWidget(self.tabWidget)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 585, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 21))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
